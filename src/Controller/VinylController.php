25e824ec2f2793a479cc36cf07d69934b51998a2
pheldu95 <phelps.duncan@gmail.com>
2023-05-08 18:12:09 -0500

    created custom doctrine query to get all mixes ordered by upvote count

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -39,7 +39,7 @@ class VinylController extends AbstractController
         $genre = $slug ? u(str_replace('-', ' ', $slug))->title(true) : null;
 
         //using doctrine to gives us mixes in order of most upvotes
-        $mixes = $mixRepository->findBy([], ['votes' => 'DESC']);
+        $mixes = $mixRepository->findAllOrderedByVotes();
 
         return $this->render('vinyl/browse.html.twig', [
             'genre' => $genre,
